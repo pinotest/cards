5d7f138b28fc2b984f1eebbd8b9ad20231305c42
pinotest <65677390+pinotest@users.noreply.github.com>
2020-06-23 08:48:35 +0200

    Del comments in main.py file

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from BusinessContact import BusinessContact
 
 fake = Faker('pl_PL')
 
-def give_time(func):
-    start = datetime.now()
-    def wrapper(*args, **kwargs):
-        return duration
-    duration = datetime.now() - start
-    print(duration)
-    return wrapper      
-
-@give_time
 def create_contacts(card_type, amount):
     '''
     Function gets two arguments: 
@@ -25,25 +16,6 @@ def create_contacts(card_type, amount):
         return [BusinessContact(fake.company(), fake.job(), fake.phone_number(),fake.first_name(),fake.last_name(), 
         fake.phone_number(), fake.email()) for i in range(amount)]
     return [BaseContact(fake.first_name(), fake.last_name(), fake.phone_number(), fake.email()) for i in range(amount)]
-# for i in range(5):
-#     ob = "ob"+str(i)
-#     ob = NewPostcard(fake.first_name() ,fake.last_name(), fake.company(), fake.job(), fake.email());
-    #print(ob.name, ob.lastname, ob.company, ob.position, ob.mail);
-    #print(ob)
-
-# card_one = BaseContact(fake.first_name() ,fake.last_name(), fake.phone_number(), fake.email());
-# card_two = BusinessContact(fake.company(), fake.job(), fake.phone_number(),fake.first_name() ,fake.last_name(), fake.phone_number(), fake.email());
-# card_three = BaseContact(fake.first_name() ,fake.last_name(), fake.phone_number(), fake.email());
-#cards = [card_one, card_two, card_three]
-
-# by_name = sorted(cards, key=lambda card: card.name)
-# by_lastname = sorted(cards, key=lambda card: card.lastname)
-# by_mail = sorted(cards, key=lambda card: card.mail)
-# print(cards)
-# print(by_name[0]);
-# print(by_lastname[0]);    
-# print(by_mail[0]);    
-cards = create_contacts(0,3000) 
-# print(cards)
-# for i,j in enumerate(cards):
-#     print(cards[i].contact())
+cards = create_contacts(0,10) 
+print(cards)
+print(cards[0].contact())
